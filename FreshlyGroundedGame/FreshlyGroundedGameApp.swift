2979f001d0655e3ddfce8b93295424a67e1d49b4
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-12 14:26:48 +0100

    Initial Commit

---
+++ FreshlyGroundedGame/FreshlyGroundedGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshlyGroundedGameApp.swift
+//  FreshlyGroundedGame
+//
+//  Created by Khalid Kamil on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FreshlyGroundedGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
