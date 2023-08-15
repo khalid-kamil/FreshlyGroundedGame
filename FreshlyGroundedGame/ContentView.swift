45ad8c721cd2fc8011139458654440572d9a801f
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-15 14:23:56 +0100

    Create basic question view

--- FreshlyGroundedGame/ContentView.swift
+++ FreshlyGroundedGame/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    let howToPlay: String = "Be vulnerable. Don't judge."
+    let questions: [Card] = Deck.defaultDeck
+    let currentQuestionIndex: Int = 2
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.white)
+                    .shadow(radius: 5)
+
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.black, lineWidth: 1)
+                    .padding(16)
+
+                Text(questions[0].prompt.uppercased())
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+            }
+            .padding(32)
+            .aspectRatio(1.0, contentMode: .fit)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        print("Restart")
+                    } label: {
+                        Image(systemName: "arrow.clockwise")
+                    }
+
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Text("\(currentQuestionIndex)/\(questions.count)")
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        print("Next Question")
+                    } label: {
+                        Text("Next Question")
+                    }
+
+                }
+            }
+            .navigationTitle("LOGO")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+
     }
 }
 
